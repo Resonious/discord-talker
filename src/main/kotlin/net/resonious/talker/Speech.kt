d93b3a97d82b18fa17e47be2e5ced902ed80e020
Nigel Baillie <metreckk@gmail.com>
2017-06-08 21:39:38 -0400

    tons of infrastructure upgrades but there's a nullpointerexcpetion presumably related to the sendingHandler being set to null by me ...

--- src/main/kotlin/net/resonious/talker/Speech.kt
+++ src/main/kotlin/net/resonious/talker/Speech.kt
@@ -2,16 +2,76 @@ package net.resonious.talker
 
 import marytts.LocalMaryInterface
 import net.dv8tion.jda.core.audio.AudioSendHandler
+import net.dv8tion.jda.core.entities.Message
 import net.dv8tion.jda.core.managers.AudioManager
+import org.apache.commons.lang.mutable.MutableByte
 
-class Speech : AudioSendHandler {
-    val mary = LocalMaryInterface()
+class Speech(
+        mary: LocalMaryInterface,
+        val message: Message,
+        val onDone: (Speech) -> Unit
+) : AudioSendHandler {
+    val text = message.strippedContent
+    var inputStream = mary.generateAudio(text)
+    var done = false
+    var ranCallback = false
+
+    init {
+        println("Received message: \"$text\"")
+
+        // Do some quick validations here
+        val myFormat = inputStream.format
+        val expectedFormat = AudioSendHandler.INPUT_FORMAT
+
+        try {
+            if (myFormat.sampleRate != expectedFormat.sampleRate)
+                throw RuntimeException(
+                    "Sample rates don't match (generated ${myFormat.sampleRate}," +
+                    "expected ${expectedFormat.sampleRate})"
+                )
+
+            if (myFormat.frameSize * 2 != expectedFormat.frameSize)
+                throw RuntimeException(
+                    "Frame sizes isn't right (generated ${myFormat.frameSize}, " +
+                    "expected ${expectedFormat.frameSize / 2} * 2)"
+                )
+
+            if (myFormat.isBigEndian == expectedFormat.isBigEndian)
+                throw RuntimeException(
+                    "Expected endianness to be swapped. This is an easy fix."
+                )
+        }
+        catch (e: Exception) {
+            done = true
+            throw e
+        }
+    }
 
     override fun provide20MsAudio(): ByteArray {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val result = ByteArray(3840)
+        val sample = ByteArray(2)
+
+        for (i in result.indices step 4) {
+            val amountRead = inputStream.read(sample)
+            if (amountRead < 2) {
+                done = true
+                onDone(this)
+                break
+            }
+
+            result[i]   = sample[1]
+            result[i+1] = sample[0]
+
+            result[i+2] = sample[1]
+            result[i+3] = sample[0]
+        }
+
+        return result
     }
 
     override fun canProvide(): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        if (!done) return true
+        else if (!ranCallback) { onDone(this); ranCallback = true; return false }
+        else return false
     }
 }
--- src/main/kotlin/net/resonious/talker/TalkerBot.kt
+++ src/main/kotlin/net/resonious/talker/TalkerBot.kt
@@ -1,64 +1,88 @@
 package net.resonious.talker
 
 import marytts.LocalMaryInterface
-import net.dv8tion.jda.client.events.call.voice.CallVoiceJoinEvent
-import net.dv8tion.jda.core.audio.AudioReceiveHandler
 import net.dv8tion.jda.core.entities.Guild
-import net.dv8tion.jda.core.entities.Message
 import net.dv8tion.jda.core.entities.VoiceChannel
-import net.dv8tion.jda.core.events.guild.voice.GenericGuildVoiceEvent
 import net.dv8tion.jda.core.events.guild.voice.GuildVoiceLeaveEvent
-import net.dv8tion.jda.core.events.message.MessageReceivedEvent
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent
 import net.dv8tion.jda.core.hooks.ListenerAdapter
-import net.dv8tion.jda.core.managers.AudioManager
 
-/**
- * Created by metre on 6/7/2017.
- */
 class TalkerBot(val dataSource: Data) : ListenerAdapter() {
-    var audioManagers = HashMap<Long, AudioManager>()
+    val speeches = mutableListOf<Speech>()
+    val mary = LocalMaryInterface()
+
 
     fun joinVoiceChannel(guild: Guild, channel: VoiceChannel) {
-        val manager = audioManagers.getOrElse(guild.idLong, { guild.audioManager })
+        val manager = guild.audioManager
 
-        // Talker really only works with one voice channel lol
-        if (manager.connectedChannel?.id != channel.id) {
-            // TODO set sending handler RIGHT HERE
+        // Talker really only works with one voice channel I guess
+        if (manager.connectedChannel?.id != channel.id)
             manager.openAudioConnection(channel)
-        }
     }
 
 
     fun leaveVoiceChannel(guild: Guild) {
-        val manager = audioManagers[guild.idLong] ?: return
+        val manager = guild.audioManager
+        speeches.clear()
         manager.closeAudioConnection()
-        audioManagers.remove(guild.idLong)
+    }
+
+
+    fun playNextSpeech() {
+        val speech       = speeches.firstOrNull() ?: return
+        val guild        = speech.message.guild
+        val voiceChannel = speech.message.member.voiceState?.channel ?: return
+
+        joinVoiceChannel(guild, voiceChannel)
+        guild.audioManager.sendingHandler = speech
+    }
+
+
+    fun speechDone(speech: Speech) {
+        speeches.remove(speech)
+        if (speeches.isEmpty())
+            speech.message.guild.audioManager.sendingHandler = null
+        else
+            playNextSpeech()
     }
 
 
     override fun onGuildMessageReceived(event: GuildMessageReceivedEvent?) {
         if (event == null) return
+        if (event.member.voiceState?.channel == null) return
         if (event.author.isBot) return
         if (!event.channel.name.contains("voice-replies")) return // TODO maybe make voice-replies name configurable
-        if (event.member.voiceState?.channel == null) return
 
-        joinVoiceChannel(event.guild, event.member.voiceState.channel)
+        speeches.add(Speech(mary, event.message, this::speechDone))
+
+        if (speeches.size == 1)
+            playNextSpeech()
     }
 
 
     override fun onGuildVoiceLeave(event: GuildVoiceLeaveEvent?) {
         if (event == null) return
         val channel = event.channelLeft ?: return
-        val manager = audioManagers[channel.guild.idLong] ?: return
+        val manager = event.guild.audioManager
 
         val isMyChannel = channel.idLong == manager.connectedChannel.idLong
 
-        if (isMyChannel && channel.members.size == 1)
+        if (isMyChannel && channel.members.size == 1) {
+            // If we're the only one left, leave!
             leaveVoiceChannel(event.guild)
+        }
+        else {
+            // If the person who left was talking, we want to remove all their speeches
+            // from the queue.
+            speeches.removeAll {
+                if (it == manager.sendingHandler)
+                    manager.sendingHandler = null
+                it.message.author.idLong == event.member.user.idLong
+            }
+
+            // In case we interrupted the current speech, start playing the next one.
+            if (manager.sendingHandler == null && speeches.size > 0)
+                playNextSpeech()
+        }
     }
-
-
-    private fun messageMentionsMe(message: Message) =
-        message.mentionedUsers.any { u -> u.id == message.jda.selfUser.id }
 }
